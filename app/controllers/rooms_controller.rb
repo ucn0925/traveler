857cfac95c00a4e5111903a055ac92c01a8793b3
ucn0925 <195886413+ucn0925@users.noreply.github.com>
2025-06-01 17:44:07 +0900

    rooms_controllerに記述

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,22 @@
 class RoomsController < ApplicationController
-end
+    before_action :authenticate_user!
+
+    def create
+        @room = Room.create(user_id: current_user.id)
+        @entry1 = Entry.create(room_id: @room.id, user_id: current_user.id)
+        @entry2 = Entry.create(params.require(:entry).permit(:user_id).merge(room_id: @room.id))
+        redirect_to "/rooms/#{@room.id}"
+    end
+
+    def show
+        @room = Room.find(params[:id])
+        if Entry.where(user_id: current_user.id, room_id: @room.id).present?
+            @messages = @room.messages
+            @message = Message.new
+            @entries = @room.entries
+            @myUserId = current_user.id
+        else
+            redirect_back(fallback_location: root_path)
+        end  #if Entry 終わり
+    end  #def show 終わり
+end  #class 終わり
